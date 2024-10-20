3a84399c6500aca9f863791a176c0fca21f5ce2f
Victoria Madrid <victoria@airops.com>
2024-10-20 20:28:38 -0300

    Always yank to clipboard

--- config/nvim/lua/settings/init.lua
+++ config/nvim/lua/settings/init.lua
@@ -4,6 +4,7 @@ vim.cmd('set tabstop=2')
 vim.cmd('set softtabstop=2')
 vim.cmd('set shiftwidth=2')
 vim.cmd('set colorcolumn=80,120')
+vim.opt.clipboard:append("unnamedplus") -- Yank to clipboard
 
 require('settings.relative_numbers')
 require('settings.trailing_spaces')
