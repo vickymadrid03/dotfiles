b7e2b0f0a993a3a02fb97114db0dd6ad30c48def
Victoria Madrid <victoria@airops.com>
2024-10-21 19:24:15 -0300

    Change how trailing spaces and indent spaces are shown

--- config/nvim/lua/settings/init.lua
+++ config/nvim/lua/settings/init.lua
@@ -6,5 +6,13 @@ vim.cmd('set shiftwidth=2')
 vim.cmd('set colorcolumn=80,120')
 vim.opt.clipboard:append("unnamedplus") -- Yank to clipboard
 
+-- Enable list mode
+vim.opt.list = true
+-- Configure listchars: tab, end-of-line, and trailing spaces
+vim.opt.listchars = {
+    tab = "› ",
+    trail = "⋅",
+    space = "⋅"
+}
+
 require('settings.relative_numbers')
-require('settings.trailing_spaces')
--- config/nvim/lua/settings/trailing_spaces.lua
+++
@@ -1,3 +0,0 @@
--- Highlight trailing spaces
-vim.api.nvim_set_hl(0, 'TrailingSpace', {ctermbg = 238, bg = '#e3c5c5'})
-vim.fn.matchadd('TrailingSpace', '\\s\\+$')
