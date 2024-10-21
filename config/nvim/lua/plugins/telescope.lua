fb9a524df979a933ed29c1b112795fd0b044dfa5
Victoria Madrid <victoria@airops.com>
2024-10-21 20:22:37 -0300

    Add keybindings to delete buffers

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -3,3 +3,17 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>/', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+local actions = require('telescope.actions')
+require('telescope').setup {
+  defaults = {
+    mappings = {
+      i = {
+        ["<c-d>"] = actions.delete_buffer,
+      },
+      n = {
+        ["dd"] = actions.delete_buffer,
+      },
+    },
+  },
+}
