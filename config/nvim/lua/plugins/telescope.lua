6ed6b1dea2f140d40a0137d7684e070134cec752
Victoria Madrid <victoria@airops.com>
2024-10-29 08:20:21 -0300

    Add keymap to search in current directory

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,8 +1,10 @@
 local builtin = require('telescope.builtin')
+local utils = require("telescope.utils")
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>/', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>ffc', function() builtin.find_files({ cwd = utils.buffer_dir() }) end, { desc = 'Telescope find files in cwd' })
 
 local actions = require('telescope.actions')
 require('telescope').setup {
